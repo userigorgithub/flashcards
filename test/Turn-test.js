2b93a7c7ce0bffa904377015e804e37c798ac68c
Igor Decess <i.decess@comcast.net>
2022-03-21 19:39:34 -0600

    Add card object for current card for test and implementation files

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -21,6 +21,10 @@ describe('Turn', function() {
     expect(turn.userGuess).to.equal('guess');
   });
 
-
+  it('should have card object for the current card in play', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.currentCard).to.deep.equal(card);
+   });
 
 });
