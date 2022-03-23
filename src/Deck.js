3b7b1ab274a3a9667b625604026b86e4735c68bb
Igor Decess <i.decess@comcast.net>
2022-03-22 20:43:54 -0600

    Add countCards method to deck test and implementation

--- src/Deck.js
+++ src/Deck.js
@@ -2,6 +2,11 @@ class Deck {
   constructor(cards) {
     this.cards = cards;
   }
+
+  countCards() {
+    console.log(this.cards.length)
+    return this.cards.length;
+  }
 }
 
 module.exports = Deck;
