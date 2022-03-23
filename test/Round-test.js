8c6747579c4409c27dffe02a15a8356571943988
Igor Decess <i.decess@comcast.net>
2022-03-23 17:50:56 -0600

    Convert to ES6 and use beforeEach due to bulkiness of code

--- test/Round-test.js
+++ test/Round-test.js
@@ -5,25 +5,34 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
-describe('Round', function() {
+describe('Round', () => {
 
-  it('should be a function', function() {
-    const round = new Round();
+  let card1;
+  let card2;
+  let card3;
+  let cards;
+  let deck;
+  let round;
+
+  beforeEach(() => {
+
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = new Card(2, 'What is a comma-separated list of related values?', ["array", "object", "function"], 'array');
+    card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ["mutator method", "accessor method", "iteration method"], 'mutator method');
+    cards = [card1, card2, card3];
+    deck = new Deck(cards);
+    round = new Round(deck);
+  });
+
+  it('should be a function', () => {
     expect(Round).to.be.a('function');
   });
 
-  it('should be an instance of Round', function() {
-    const round = new Round();
+  it('should be an instance of Round', () => {
     expect(round).to.be.an.instanceof(Round);
   });
 
-  it('should have a card in deck', function() {
-    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const card2 = new Card(2, 'What is a comma-separated list of related values?', ["array", "object", "function"], 'array');
-    const card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ["mutator method", "accessor method", "iteration method"], 'mutator method');
-    const cards = [card1, card2, card3];
-    const deck = new Deck(cards);
-    const round = new Round(deck);
+  it('should have a card in deck', () => {
     expect(round.deck.cards[0]).to.equal(card1);
   });
 
